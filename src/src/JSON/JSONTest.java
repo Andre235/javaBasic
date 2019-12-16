9412b85091274434f35c3a795f61169c39b7a421
zhaojingchao <2356430623@qq.com>
2019-12-17 07:39:42 +0800

    2019-12-17 JSONTest

---
+++ src/src/JSON/JSONTest.java
@@ -0,0 +1,17 @@
+package JSON;
+
+import java.util.ArrayList;
+
+/**
+ * @author : 赵静超
+ * @date : 2019/12/15
+ * @description :
+ */
+public class JSONTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+    }
+}
